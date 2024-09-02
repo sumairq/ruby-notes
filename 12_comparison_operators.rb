00ee8454d4e0d557fee7768f9a1e80920b79ef0d
sumairq <sumairqaisar992@gmail.com>
2024-09-02 13:48:58 +0500

    Add lesson 12 comparison operators

---
+++ 12_comparison_operators.rb
@@ -0,0 +1,24 @@
+# Comparison Operators
+
+=begin
+  Comparison Operators: 
+    > greater than
+    < less than
+    >= greater than or equal to
+    <= less than or equal to 
+    == equal to
+    != not equal to
+=end
+puts "\n\n\n"
+
+a = 100
+b = 10
+c = 10
+
+puts a > b # true
+puts a < b # false
+puts a <= c # false
+puts b >= c # true
+puts b == c # true
+puts a != c # true
+
