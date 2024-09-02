f355173cf790ffdaf4497d65145a0ac0068c03d6
sumairq <sumairqaisar992@gmail.com>
2024-09-02 13:32:56 +0500

    Add math methods lesson 10

---
+++ 10_math_methods.rb
@@ -0,0 +1,13 @@
+# Math Methods
+puts "\n\n\n"
+
+a = -766.4
+puts a.abs()  #Absolute value
+puts a.round() #Rounded value
+
+b = 576.2
+puts b.floor() #Round up
+puts b.ceil() # Round down
+
+puts Math.sqrt(4)  # Square root
+puts Math.log(100) # log with base e  (e is the default second parameter you can change this )
