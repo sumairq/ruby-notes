5afac92672dbc919141ace36365356fb50c5a3f7
sumairq <sumairqaisar992@gmail.com>
2024-09-02 12:15:28 +0500

    Add lesson 02 Data Types

---
+++ 02_data_types.rb
@@ -0,0 +1,22 @@
+# Data Types
+
+=begin
+  Data types are: String, Number, Float, Boolean, 
+  Nil, Arrays, Hashes, Symbols
+
+
+=end
+puts "\n\n\n"
+
+my_num = 25       #Integer
+my_boolean = true  #Boolean
+my_string = "Ruby"  #String
+my_float = 3.14   #Float
+
+
+puts my_num
+puts my_boolean
+puts my_string
+puts my_float
+
+
