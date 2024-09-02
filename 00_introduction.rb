5bb6325ec5b473584427433e13bfd3807da1eacb
sumairq <sumairqaisar992@gmail.com>
2024-09-02 11:57:34 +0500

    Add lesson 00

---
+++ 00_introduction.rb
@@ -0,0 +1,4 @@
+# Ruby is a dynamic, reflective, object oriented programming language.
+# general purpose programming language.
+# Favours developer's productivity over machine's
+# Feels like english language
