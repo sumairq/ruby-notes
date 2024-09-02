0e7096f3dcacf61928d3db7b4a30e06213f938bc
sumairq <sumairqaisar992@gmail.com>
2024-09-02 12:31:20 +0500

    Add lesson 04 string interpolation

---
+++ 04_interpolation.rb
@@ -0,0 +1,15 @@
+#Math: String Interpolation
+
+
+=begin
+   Adding variables to a string.
+=end
+
+puts "\n\n\n"
+
+name = "Rajat"
+
+puts "Hello, #{name}!"
+puts 'Hello, #{name}!'   #Does not work with single quotes
+
+puts "Hello, " + name + "!"
