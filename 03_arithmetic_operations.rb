739589ab88e0782a2ee6c141cb1876cc20468972
sumairq <sumairqaisar992@gmail.com>
2024-09-02 12:25:04 +0500

    Add lesson 03 arithmetic operations

---
+++ 03_arithmetic_operations.rb
@@ -0,0 +1,34 @@
+# Math : Arithmetic Operations
+
+=begin
++ addition
+- subtraction
+* multiplication
+/ division
+% modulo (remainder)
+** exponential (power)
+=end
+
+puts "\n\n\n"
+
+a = 7;
+b = 2;
+
+
+puts a + b
+
+puts a - b
+
+puts a * b
+
+puts a % b
+
+puts a ** b
+
+puts a / b
+
+c = 9.0
+d = 4
+
+puts c / d
+
