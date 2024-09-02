ec813c6dcd56fee355d0d9444c4d227efaf50373
sumairq <sumairqaisar992@gmail.com>
2024-09-02 13:25:14 +0500

    Add 09 assignment operators

---
+++ 09_assignment_operators.rb
@@ -0,0 +1,25 @@
+# Assignment Operators
+
+
+=begin
+Assignment Operators:
+  +=, -=, *=, /=
+=end
+puts "\n\n\n"
+
+a = 100
+b = 10
+puts a += b # a = a + b
+
+a = 100
+b = 10
+puts a -= b # a = a - b
+
+a = 100
+b = 10
+puts a *= b # a = a * b
+
+a = 100
+b = 10
+puts a /= b # a = a / b
+
