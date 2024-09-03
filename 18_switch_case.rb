8ffe23dc1c4b6bdf89de3537028f6cb98f50229e
sumairq <sumairqaisar992@gmail.com>
2024-09-03 14:57:24 +0500

    Add switch case statements

---
+++ 18_switch_case.rb
@@ -0,0 +1,28 @@
+# Switch case statement
+puts "\n\n\n"
+
+age = 30
+case age
+  when 0..18
+    puts "Study"
+  when 18..60
+    puts "Job"
+  when 60..100
+    puts "Retire"
+  else
+    puts "Dead"
+  end
+
+  #puts "Hello There!"
+  # greeting =gets.chomp
+  # case greeting
+  #  when "English" then puts "hello!"
+  #  when "French" then puts "Bonjour!"
+  #  when "German" then puts "Guten Tag!"
+  #  when "Finish" then puts "Haloo!"
+  # else puts "I don't know that language!"
+# end
+
+
+
+# "then" syntax is totally optional but it makes the code read like english 
