853ba9747ddb0379f11fc74dafd2eda57932308c
sumairq <sumairqaisar992@gmail.com>
2024-09-05 21:12:06 +0500

    Finish Symbols lesson

--- 22_symbols.rb
+++ 22_symbols.rb
@@ -11,8 +11,21 @@ Useful as keys mainly
 =end
 puts "\n\n\n"
 
-actors = {
-    :tom_cruise => "Mission Impossible",
-    :rock
-}
+# actors = {
+#     "tom_cruise" => "Mission Impossible",
+#     :rock => "Red Notice",
+#     :dev_patel => "Hotel Mumbai"
+# }
+
+# puts actors
+
+
+# puts "\n\nString ids change"
+# puts "hello".object_id
+# puts "hello".object_id
+
+# puts "\n\nSymbols object ids don't change as they have..."
+puts :hello.object_id
+puts :hello.object_id
+
 
