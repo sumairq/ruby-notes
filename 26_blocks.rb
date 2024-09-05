95fdb3bcad7dcb22e824a9534340b060fc4f8840
sumairq <sumairqaisar992@gmail.com>
2024-09-05 22:23:12 +0500

    Basic notes for blocks

---
+++ 26_blocks.rb
@@ -0,0 +1,42 @@
+# Blocks in ruby
+
+=begin
+    Blocks are like methods without a name, they are anonymous functions
+    Allows to write powerful flexible code.
+    A block is a piece of code that accepts arguments and returns a value.
+    A block is always passed to a method calll.
+    Blocks are not objects
+    
+
+    {...} and do ... end
+=end
+
+# Using do end block , used normally for multi-line code.
+1.times do 
+    puts "I'm do end block"
+end
+
+# Using curly braces {} block, used normally for single-line code.
+1.times { puts "I'm {} block"}
+
+# puts "\n\n*****************************"
+# puts "WITH ARGUMENTS"
+# puts "\n\n*****************************"
+
+# # Block with arguments
+
+# Block with arguments
+[1,2,3,4,5].each do |index|
+    puts index
+end
+
+[1,2,3,4,5].each { |number| puts "#{number} was passed to the block"}
+
+# puts "\n\n*****************************"
+# puts "RETURNS VALUE"
+# puts "\n\n*****************************"
+
+
+
+
+
