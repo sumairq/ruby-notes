1074fe028b1ad3daf6e1bc34a2a78248efe6d7ff
sumairq <sumairqaisar992@gmail.com>
2024-09-05 21:00:04 +0500

    Add basic explanation for symbols

---
+++ 22_symbols.rb
@@ -0,0 +1,18 @@
+# Symbols
+
+=begin
+Symbols are like strings , exept they are code.
+- Have better performance than strings
+- Takes less memory than strings
+- unique copy
+- immutable
+
+Useful as keys mainly
+=end
+puts "\n\n\n"
+
+actors = {
+    :tom_cruise => "Mission Impossible",
+    :rock
+}
+
