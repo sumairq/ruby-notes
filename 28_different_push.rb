331fb9e4217081619972663e81ead290ba29a3ee
sumairq <sumairqaisar992@gmail.com>
2024-09-06 18:10:35 +0500

    Add lesson : a different push

---
+++ 28_different_push.rb
@@ -0,0 +1,11 @@
+# Instead of push method we can also use <<
+puts "\n\n\n"
+
+alphabet = ["a", "b", "c"]
+alphabet << "d"
+print alphabet
+
+puts "\n"
+caption = "A giraffe surrounded by"
+caption << "weezards!" # We cal also use it for string concategnation
+print caption
