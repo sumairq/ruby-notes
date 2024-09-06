a07ddf30c81f24970320a425bba78d6edd0e150d
sumairq <sumairqaisar992@gmail.com>
2024-09-06 23:08:19 +0500

    Add lesson 30 proc examples

--- 30_proc.rb
+++ 30_proc.rb
@@ -21,3 +21,36 @@ print (1..100).to_a.select(&multiples_of_5)
 
 # numbers_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # numbers_2 = [10, 11, 12, 13, 14, 15, 16, 17]
+
+# even_numbers_1 = numbers_1.select(&is_even)
+# even_numbers_2 = numbers_2.select(&is_even)
+
+# puts "Even numbers in list 1: #{even_numbers_1}"
+# puts "Even numbers in list 2: #{even_numbers_2}"  
+
+# puts "\n\n\n"
+
+cube = Proc.new { |x| x**3 }
+print [1, 2, 3].collect!(&cube)
+
+# puts "\n\n"
+
+# hello = Proc.new do
+#   puts "Hello Rajat"
+# end
+
+do
+   puts "Hello Rajat!"
+end
+
+hello.call
+
+
+# puts "\n\n"
+
+# number_array = [1, 2, 3, 4, 5, 6, 7, 9, 10]
+# strings_array = numbers_array.map(&:to_s)
+# print strings_array
+
+
+
