b06aaaa778e2799f360882a9792f90e2da83933a
sumairq <sumairqaisar992@gmail.com>
2024-09-07 10:33:09 +0500

    Finish lambda lesson

--- _lambda.rb
+++ _lambda.rb
@@ -16,3 +16,31 @@ puts my_lambda.call(3) # Output: 6
 my_proc = Proc.new {|x| x * 2}
 puts my_proc.call(3)  #Output: 6
 
+# # Additional Argument
+# lambda_example = ->(x) {puts x}
+# proc_example = Proc.new { |x| puts x}
+
+# lambda_example.call(1, 2) #Output: ArgumentError: wrong number of arguments
+#proc_example.call(1, 2) # Output: 1
+
+# # Return Behaviour
+
+def method_with_lambda
+    my_lambda = -> { return "Return from lambda" }
+    # control comes back here
+    text = my_lambda.call
+    puts text
+    return "Method continues after lambda"
+end
+
+def method_with_proc
+    my_proc = Proc.new (return "Returned from proc")
+    # control does not comes back here
+    text = my_proc.call
+    puts text
+    return "Method continues after proc"
+end
+
+puts method_with_lambda
+puts "\n\n"
+puts: "Random: " + method_with_proc
