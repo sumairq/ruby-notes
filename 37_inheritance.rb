63ddd5b0a31e7811d720732d03eb56963e7b6e56
sumairq <sumairqaisar992@gmail.com>
2024-09-08 12:45:00 +0500

    Finish inheritance explanation

--- 37_inheritance.rb
+++ 37_inheritance.rb
@@ -1,1 +1,45 @@
 # Inheritance
+puts "\n\n\n"
+
+class Animal
+    def speak
+        "An animal makes a sound."
+    end
+end
+
+
+class Dog < Animal
+    def speak
+        "Woof!"
+    end
+end
+
+class Cat < Animal
+    def speak
+        super
+        "Meow!"
+    end
+end
+
+class UnknownAnimal < Animal
+end
+
+
+animal = Animal.new
+puts animal.speak # Output: An animal makes a sound
+
+
+dog = Dog.new
+puts dog.speak # Output: Woof!
+
+
+cat = Cat.new
+puts cat.speak # Output: Meow!
+
+
+unknown = UnknownAnimal.new
+puts unknown.speak
+
+# TODO: Now use super to override
+
+
